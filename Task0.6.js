0f1d597aaaad5f69ea292dfc990b1492de79c947
Lelana1 <matobelaw@gmail.com>
2021-04-20 00:18:49 +0200

    Added Task0.6.js

---
+++ Task0.6.js
@@ -0,0 +1,12 @@
+//Write a function that takes in three numbers and returns the maximum number.
+
+function maximum(num1, num2, num3) {
+    if (num1 > num2) {
+        return num1;
+
+    } else if (num2 > num3) {
+        return num2;
+
+    } else {
+        return num3;}
+}
