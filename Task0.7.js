f56addbdee8fe30523a5b38972f3580e6cba2b3e
Lelana1 <matobelaw@gmail.com>
2021-04-19 18:19:00 +0200

    Added Task0.7.js

---
+++ Task0.7.js
@@ -0,0 +1,2 @@
+/*Write a function that takes in a number representing the temperature in Celsius and returns the temperature in Fahrenheit. 
+Write another function that does the opposite (Fahrenheit to Celsius)*/
