037b223868c38cc8df04a80329968c67aca5df99
Lelana1 <73743485+Lelana1@users.noreply.github.com>
2021-04-22 12:20:48 +0200

    Update Task0.9.js

--- Task0.9.js
+++ Task0.9.js
@@ -1,14 +1,20 @@
 /*Write a function that takes in a string and then prints out all the vowels in the string. 
 Make sure it can deal with capital letters and small letters.*/
 
-function vowels(str) {
-    return str.match(/[aeiou]/ig, '');
+function vowelsInAstring(str) {
+    
+    const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    var vowelsInstr = str.split("");
+    var vowelfound = [];
+
+    for (var i in vowelsInstr) { 
+        if (vowels.includes(vowelsInstr([i])) 
+        (vowelfound.push(vowelsInstr[i])
+    }  
+
+        
+    
+    
  
+    
 }
-
-function vowels1(str) {
-    return str.match(/[^aeiou]/ig, '');
-
-
-}
-
