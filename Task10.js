f09cc4409072d512b9e0024d117c4339961de591
Lelana1 <73743485+Lelana1@users.noreply.github.com>
2021-04-29 23:26:20 +0200

    Update Task10.js
    addressed changes

--- Task10.js
+++ Task10.js
@@ -3,16 +3,18 @@ and outputs the common characters/letters that they share.
 (For example, Input: ‘house’, ‘computers’ . Output: ‘Common letters: o, u, e, s’)*/
 
 function commonLetters(str1, str2) {
-    var commonChar = []
-    let i, j, part;
-     for( i = 0; i < str1.length - 1; i++) {
-         for (j = i + 1; j <= str1.length; j++) {
-             part = st1.slice(i, j);
-                 if(str2.indexOf(str1[1] !== -1));
-                 commonChar.push(str1[i]);
-             }
+const commonChar = []
+    var  i; 
+    var j;
+
+        for( i = 0; i < str1.length; i++) {
+         for (j = 0; j < str2.length; j++) {
+            if(str1.indexOf(str2[i]) == i++);
+            commonChar.push(str1[i]);
+                
          }
-     }
- 
-     //i dont know how to solve this
+             
+        return commonChar;  
+        }
+    } 
      
