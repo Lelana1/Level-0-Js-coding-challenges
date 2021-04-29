398cb6fc80d152a8fb4aaa212941107a8e6a52e2
Lelana1 <73743485+Lelana1@users.noreply.github.com>
2021-04-29 21:15:24 +0200

    Update Task0.9.js
    Addressed changes

--- Task0.9.js
+++ Task0.9.js
@@ -2,19 +2,17 @@
 Make sure it can deal with capital letters and small letters.*/
 
 function vowelsInAstring(str) {
+    const input = str.toLowerCase();
+    const vowels = ["a", "e", "i", "o", "u"];
     
-    const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
-    var vowelsInstr = str.split("");
-    var vowelfound = [];
+    var vowelsfound = [];
 
-    for (var i in vowelsInstr) { 
-        if (vowels.includes(vowelsInstr([i])) 
-        (vowelfound.push(vowelsInstr[i])
-    }  
-
+    for (let i = 0; i < input.length; i++) {
+        if(vowelsfound.includes(input[i])) continue; 
+        if(vowels.includes(input[i])) (vowelsfound.push(input[i]));
+     
+        Output = " Vowels" + ": "  + (vowelsfound) + " ,";
         
-    
-    
- 
-    
-}
+    } 
+       
+    }
