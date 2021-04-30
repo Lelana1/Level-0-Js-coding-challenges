2364de09cf910490e2579bbc05e85a681952329c
Lelana1 <73743485+Lelana1@users.noreply.github.com>
2021-04-30 08:51:12 +0200

    Update Task0.9.js
    removed comments

--- Task0.9.js
+++ Task0.9.js
@@ -1,6 +1,3 @@
-/*Write a function that takes in a string and then prints out all the vowels in the string. 
-Make sure it can deal with capital letters and small letters.*/
-
 function vowelsInAstring(str) {
     const input = str.toLowerCase();
     const vowels = ["a", "e", "i", "o", "u"];
