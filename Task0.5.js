69ceeaf7d80514fad24aac55f301135a68a2e096
Lelana1 <73743485+Lelana1@users.noreply.github.com>
2021-04-30 09:42:29 +0200

    Update Task0.5.js
    removed comments

--- Task0.5.js
+++ Task0.5.js
@@ -1,10 +1,6 @@
-/*Write a function that takes in three numbers. 
-These numbers represent the lengths of the sides of a triangle. 
-The function should return the area of a triangle*/
-
-function areaOfAtriangle(x, y,z) {             //Heron's formula
+function areaOfAtriangle(x, y,z) {            
     s = (x + y + z) /2;
-    Area = Math.sqrt(s*((s-x)*(s-y)*(s-z)));   //{Area}}={\sqrt  {s(s-a)(s-b)(s-c)}},
+    Area = Math.sqrt(s*((s-x)*(s-y)*(s-z)));   
 
     return Area;
 
