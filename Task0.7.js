d1e0e3d4504e71160d802dce7a21201761a8a4c4
Lelana1 <73743485+Lelana1@users.noreply.github.com>
2021-04-30 08:50:25 +0200

    Update Task0.7.js
    removed comments

--- Task0.7.js
+++ Task0.7.js
@@ -1,6 +1,3 @@
-/* Write a function that takes in a number representing the temperature 
-in Celsius and returns the temperature in Fahrenheit. 
-Write another function that does the opposite (Fahrenheit to Celsius)*/
 function CelsiusTofahrenheit(Fahrenheit) {
     let Fahrenheit = Celsius * (5/9) + 32;
         return Fahrenheit;
