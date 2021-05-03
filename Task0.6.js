fcce8720b7f56d99e8ab28bd48eb059323f5ea81
Lelana1 <73743485+Lelana1@users.noreply.github.com>
2021-05-03 23:38:42 +0200

    updated per review

--- Task0.6.js
+++ Task0.6.js
@@ -1,11 +1,10 @@
 
 function maximum(num1, num2, num3) {
-    if (num1 > num2 || num1 > num3) {
-        console.log(num1);
-
-    } else if (num2 > num3 && num1) {
-        console.log(num2);
-
-    } else {
-        console.log(num3)
-    }
+  if (num1 > num2 && num1 > num3) {
+    return num1;
+  } else if (num2 > num3 && num1) {
+    return num2;
+  } else {
+    return num3;
+  }
+}
