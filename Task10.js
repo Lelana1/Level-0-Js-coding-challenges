a849d2399a58b7cfe53672a49beca6e15609b337
Lelana1 <73743485+Lelana1@users.noreply.github.com>
2021-05-04 13:36:32 +0200

    Update Task10.js
    updated per review

--- Task10.js
+++ Task10.js
@@ -1,20 +1,13 @@
-/*Make a function that takes two strings as input, 
-and outputs the common characters/letters that they share. 
-(For example, Input: ‘house’, ‘computers’ . Output: ‘Common letters: o, u, e, s’)*/
-
 function commonLetters(str1, str2) {
-const commonChar = []
-    var  i; 
-    var j;
+  const commonChar = [];
+  for (let i = 0; i < str1.length; i++) {
+    for (let j = 0; j < str2.length; j++) {
+      if (str1[i] == str2[j]) {
+        commonChar.push(str1[i]);
+      }
+    }
+  }
+  return "Common letters: " + commonChar.join(", ");
+}
 
-        for( i = 0; i < str1.length; i++) {
-         for (j = 0; j < str2.length; j++) {
-            if(str1.indexOf(str2[i]) == i++);
-            commonChar.push(str1[i]);
-                
-         }
-             
-        return commonChar;  
-        }
-    } 
      
