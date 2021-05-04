732979c41b9451632004810332c9318a8ff2f2c6
Lelana1 <73743485+Lelana1@users.noreply.github.com>
2021-05-04 13:14:21 +0200

    Update Task0.8.js
    updated changes per review

--- Task0.8.js
+++ Task0.8.js
@@ -1,13 +1,17 @@
 function timeConverter(num) {
 var hour = Math.floor(num / 60); 
 var minute = Math.round(num % 60); 
-   
+   let hourString = " hour";
+   let minuteString = " minute";
 let output = ""; 
     
-    if (hour > 1 && minute > 1) output += `${hour} hours, ${minute} minutes` ;
-    if (hour > 1 && minute < 2) output += `${hour} hours, ${minute} minute` ;
+    if (hour > 1 || hour ==0){
+        hourString += "s";
+    }
+    if (minute > 1 || minute == 0){
+        minuteString += "s";
+    }
 
-    if ((hour < 2, minute > 1)) output += `${hour} hour, ${minute} minutes` ;  
- 
-     
-}    
+    output += hour + hourString + ", " + minute + minuteString;
+     return output;
+}
