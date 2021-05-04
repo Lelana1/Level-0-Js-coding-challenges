ab8f77624b439ffdf2a900d7377bc99c4c9ec02b
Lelana1 <73743485+Lelana1@users.noreply.github.com>
2021-05-04 13:20:03 +0200

    Update Task0.9.js

--- Task0.9.js
+++ Task0.9.js
@@ -1,15 +1,16 @@
 function vowelsInAstring(str) {
-    const input = str.toLowerCase();
-    const vowels = ["a", "e", "i", "o", "u"];
-    
-    var vowelsfound = [];
+  const input = str.toLowerCase();
+  const vowels = ["a", "e", "i", "o", "u"];
 
-    for (let i = 0; i < input.length; i++) {
-        if(vowelsfound.includes(input[i])) continue; 
-        if(vowels.includes(input[i])) (vowelsfound.push(input[i]));
-     
-        Output = " Vowels" + ": "  + (vowelsfound).join(", ");
-        
-    } 
+  var vowelsfound = [];
+
+  for (let i = 0; i < input.length; i++) {
+    if (vowelsfound.includes(input[i])) continue;
+    if (vowels.includes(input[i])) vowelsfound.push(input[i]);
+
+    output = "Vowels" + ": " + vowelsfound.join(", ");
+  }
+  console.log(output);
+}
+
        
-    }
